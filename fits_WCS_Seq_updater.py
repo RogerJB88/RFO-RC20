f1578941dbfa4ba324e41d1f7c55834984e09815
Roger J Boulanger <rboulanger@rfo.org>
2025-05-23 13:38:43 -0700

    Update fits_WCS_Seq_updater.py
    Added Dark file calibration instead of the interim sythetic DARK.

--- fits_WCS_Seq_updater.py
+++ fits_WCS_Seq_updater.py
@@ -3,7 +3,7 @@
 fits_WCS_updater.py
 
 By: Roger Boulanger
-Date:  04/29/2025   xxxx03/16/2025   xxxx03/14/2025  xxxx02/24.2025     xxxxx02/04/2025
+Date:  05/22/2025   Alpha 5.22
 
         ***** THIS PROGRAM IS EXPERIMENTAL CODE NOT INTENDED FOR PRODUCTION! *****
 
@@ -38,110 +38,112 @@ from numpy import float32
 import logging
 
 
-# The calibrateImage function is called by the add_WCS_Coordinates() function. "inpath" is the path to the image file to be
-# calibrated and "flatpath" is the folder containing available FLAT calibration files. This folder will be searched for a
-# FLAT file with matching Filter and Binning settings. The calibration formula used is: (LIGHT - DARK) / (FLAT - BIAS).
-# Since the RC20 monocamera sensor has zero amp glow or fixed pattern anomalies, synthetic vales of BIAS and DARK are derived
-# from the ASI2600MM Specifications and to be confirmed by empirical analysis.
-def calibrateImage (inpath, flatpath):
+
+def getMstrDrk(darkpath):
+    with fits.open(darkpath+'\\Dark_Master-c.fits') as fdrk:
+        dark_hdr = fdrk[0].header
+        dark_exp = float32(dark_hdr['EXPTIME'])
+        dark_binning = int(dark_hdr['XBINNING'])
+        # scale the data 0.000 to 1.000
+        mstr_dark = (float32(fdrk[0].data))/65535.0
+
+        # Sythetic bias is derived from the camera adu offset
+        bias = float32( dark_hdr['OFFSET'] * 10 + 3.0) / 65535.0
+	
+    return [dark_exp, dark_binning, bias, mstr_dark]
+
+
+'''
+ The calibrateImage function is called by the add_WCS_Coordinates() function. "ip_dir' is the working directory and "calinpath" is the path to the image file to be calibrated. This folder will be searched for a FLAT file with matching Filter and Binning settings. The calibration formula used is: (LIGHT - DARK) / (FLAT - BIAS). A synthetic value of BIAS is derived from the ASI2600MM Specifications and serves until bias files are collected.
+'def calibrate(ip_dir, calinpath, dark_exp, dark_binning, bias, mstr_dark):
     try:
-        hdul = fits.open (inpath, 'update')
-        # Get image file header to determine type of image (ie, LIGHT, DARK, FLAT, etc)
-        hdr = hdul[0].header
-        imageType = hdr['IMAGETYP'].upper()
-
-        if not (imageType.startswith ('LIGHT') ):
-            logger.info('NOT a LIGHT')
-            hdul.close()
-            return 1
-        else:
-            imgFilter = hdr['FILTER']
-            binning = int(hdr['XBINNING'])
-            gotFlat = False
-            # Try to find an appropriate Flat file...
-            try:
-                for entry in os.scandir(flatpath):
-                    logger.info(entry.name)
-                    if not entry.name.startswith ('.') and entry.name.endswith('fits'):
-                        with fits.open (flatpath + '/' + entry.name) as f :
-                            flt = f[0].header
-                            calType = flt['IMAGETYP'].upper()
-
-                            if (calType.startswith('FLAT') and flt['FILTER'] == imgFilter and int(flt['XBINNING']) == binning):
-                               
-                                flat_data = float32(f[0].data) / 65535.0
-                                gotFlat = True
-                                logger.info('Flat file found')
-                                break
-                            else:
-                                logger.info('looping')                            
-            except Exception as e :
-                logger.error("ERROR " + str(e) +'\n')
-                logger.error( "Can't load Flat for "+ inpath)
-                hdul.close()
-                return 2
-            if not (gotFlat) :
-                logger.error("No matching Flat for " + inpath)
-                hdul.close()
-                return 3
-                
-            # Normalize the integer image data resulting in values 0.000000 to 1.000000
-            image_data = float32(hdul[0].data) / 65535.0
-            medImg = np.mean(image_data)
-            logger.info('IMG MEAN= '+str(medImg))
-
-            x, y = (image_data.shape)
-            xf, yf = (flat_data.shape)
-                        
-            logger.info ('yf = ' + str(yf))
-            # if flat file came from SkyX, trim it to 1044 x 1560
-            if (yf > y) :
-                flat_data = flat_data[0:1044, 1:1561]
-
-            # Sythetic bias is derived from the camera adu offset
-            bias = float32( hdr['OFFSET'] * 10 + 2.0) / 65535.0
-            for x in flat_data:
-                x -= bias            # subtract bias from flat
-
-            # Synthetic dark is derived from camera adu offset + noise floor estimate at diff exposures
-            exp = hdr['EXPTIME']            # image exposure time
-            if (exp <= 120): exp = 0         # no factor at 120s or less exposure
-            expFactor = 2 + exp * 0.0040
-
-            darkBias = (hdr['OFFSET'] * 10.0 + expFactor)/65535.0       # 10 is camera adu mult factor
-            for x in image_data:
-                x -= darkBias             # subtract  dark from image
+       
+        try:
+            fimg = fits.open (calinpath, 'update')
+        except:
+            logger.error(" Can't open " + calinpath)
+            return 2
             
-            medFlat = np.mean(flat_data)
-            #logger.info('medImg= '+str(medImg))
-
-            cal_data = float32(image_data / flat_data)
-            #logger.info (cal_data)
-            maxCal = np.max(cal_data)
-            maxImg = np.max(image_data)
-            medCal = np.mean(cal_data)
-
-            #normalize the data to match original image mean adu
-            #norm_data = cal_data  * medImg / medCal
-            norm_data = cal_data  * maxImg / maxCal
-
-            hdul[0].data = np.int16((norm_data * 65535.0) - 32768)
-            hdr['CALSTAT'] = 'BDF'
-            hdr['BZERO'] = 32768
-            hdr['BSCALE'] = 1
-            hdul.close()
-        return 0
+        hdr = fimg[0].header
+        img_binning = int(hdr['XBINNING'])
+        img_type = hdr['IMAGETYP'].upper()
+        img_filter = hdr['FILTER']
+     
+        imgbuf = (float32(fimg[0].data))/65535.0
+        img_exp = float32(hdr['EXPTIME'])
+        dark_buf = mstr_dark 
         
+        # If more than 40s exposure difference..
+        if (abs(img_exp - dark_exp) > 40):
+            exp_scaleFactor = float32((img_exp - dark_exp) * 0.0013 + 1)
+            logger.info('expScaleFactor = '+str(exp_scaleFactor))
+            hotpix_threshold = float32(1024.0/65535.0)
+            for x in dark_buf:
+                x -= bias
+                chk = (x < hotpix_threshold)
+                x[chk] *= exp_scaleFactor
+                x += bias
+                 
+        #img_median = np.median(imgbuf)
+        #logger.info("IMGMEDIAN="+str(img_median))
+        diff_img= np.subtract(imgbuf, dark_buf)
+        # Eliminate unrealistic numbers...
+        for x in diff_img:
+            #chk = (x < img_median)
+            #x[chk] = img_median            
+            chk = (x < 0)
+            x[chk] = 0
+
+        try:
+            got_flat = False
+            flatpath = ip_dir + '\\FLATS'
+            for flat in os.scandir(flatpath):
+               #logger.info(flat.name)
+                if flat.name.startswith ('F') and flat.name.endswith('.fits'):
+                    with fits.open (flatpath + '\\' + flat.name) as f :
+                        flt_hdr = f[0].header
+                        calType = flt_hdr['IMAGETYP'].upper()
+
+                        if (calType.startswith('FLAT') and flt_hdr['FILTER'].startswith(img_filter) and flt_hdr['XBINNING'] == img_binning):
+                            flat_data = float32(f[0].data) / 65535.0
+                            got_flat = True
+                            logger.info('Flat file found')
+                            break
+                                            
+                    except Exception as e :
+            logger.error("ERROR " + str(e) +'\n')
+            logger.error( "Can't load Flat for "+ calinpath)
+            fimg.close()
+            return 3
+
+        if not (got_flat) :
+            logger.error("No matching Flat for " + calinpath)
+            fimg.close()
+            return 4
+
+
+        flat_data -= bias
+
+        maxFlat = np.max(flat_data)
+        flat_data /= maxFlat
+        diff_img /= flat_data
+        f[0].data = np.int16(diff_img * 65535 - 32768)
+        hdr['CALSTAT'] = 'BDF'
+        hdr['BZERO'] = 32768
+        hdr['BSCALE'] = 1
+        fimg.close()
+        f.close()
+        return 0
+
     except Exception as e :
-        logger.error("ERROR " + str(e) +'\n')
-        logger.error("Failed calibration " + inpath)
-        hdul.close()
-        return 4
+        print('ERROR= '+str(e)+'\n')
+        logger.error('ERROR= '+str(e)+'\n') 
+        return 1
 
 
-# The gen_seqNbr function rplaces the NINA generated file sequence number with and RFO standard
-# continuously incrementing eight digit sequence number. This overcomes the problem of NINA's
-# numbers being reset to zero at startup. It is called by the add_WCS_coordinates() function.
+'''
+The gen_seqNbr function rplaces the NINA generated file sequence number with and RFO standard  continuously incrementing eight digit sequence number. This overcomes the problem of NINA's numbers being reset to zero at startup. It is called by the add_WCS_coordinates() function.
+'''
 def gen_seqNbr(img, seqNbr):
     # replace NINA seq nbr with master sequential number...
     try:
@@ -153,55 +155,55 @@ def gen_seqNbr(img, seqNbr):
     except  Exception as e:
         logger.error("ERROR Numbering" + str(e) +'\n')
         return img
+'''        
         
-# The add_WCS_Coordinates function first calls the gen_seqNbr function to re-number all NINA
-# generated image files. Then with LIGHT files only it plate-solves the image using ASTAP plate
-# solver in order to add WCS coordinates to the image header and if successful, designate it as 
-# having been plate solved and written to the wcs_dest folder. An unsuccessful plate-solve results
-# in the file being written to the nowcs_dest folder and no further action is taken on this file.
-# if successful the file is then copied but with a leading "MNc..." to the filename and then
-# sent to the calibrateImage function to be calibrated. If the "MNc...." file has been successfully
-# calibrated its header is modified to indicate its calibrated status and then it is written to 
-# the wcs_dest folder. If not successful the file is deleted.
-def add_WCS_Coordinates (ip_dir, wcs_dest, nowcs_dest, flatpath):
+The add_WCS_Coordinates function first calls the gen_seqNbr function to re-number all NINA generated image files. Then with LIGHT files only it plate-solves the image using ASTAP plate solver in order to add WCS coordinates to the image header and if successful, designate it as having been plate solved and written to the wcs_dest folder. An unsuccessful plate-solve results in the file being written to the nowcs_dest folder and no further action is taken on this file. if successful the file is then copied but with a leading "MNc..." to the filename and then sent to the calibrateImage function to be calibrated. If the "MNc...." file has been successfully calibrated its header is modified to indicate its calibrated status and then it is written to the wcs_dest folder. If not successful the file is deleted.
+'''
+def add_WCS_Coordinates (ip_dir, wcs_dest, nowcs_dest):
     try:
         with open (ip_dir+'\\SEQ_NBR\\nina_seqNbr.txt', 'r') as fs:
             seqNbr = int (fs.readline() )
     except:
-        seqNbr = 0       
+        seqNbr = 0
+    try:
+        darkpath = ip_dir+'\\DARKS'
+        
+        [dark_exp, dark_binning, bias, mstr_dark] = getMstrDrk(darkpath)
+    except:
+        logger.error("Cannot get: "+darkpath+'\\xxxx')
+        return 5
+       
     try:
         for entry in os.scandir (ip_dir):
             imgname = entry.name
             inpath = str(ip_dir) + '\\'+imgname
-            #print('0: '+inpath)
-            if not imgname.startswith ('.') and imgname.endswith('fits'):
+
+            if imgname.startswith ('MN ') and imgname.endswith('.fits'):
                 
-                logger.info ("INITIAL INPATH: "+inpath)
+                #logger.info ("INITIAL INPATH: "+inpath)
                 try:
                     hdul = fits.open (inpath)
                     # Get image file header to determine type of image (ie, LIGHT, DARK, FLAT, etc)
                     hdr = hdul[0].header    
                     imageType = hdr['IMAGETYP'].upper()
-                    #print('1: '+imageType)
                     try:
                         swcreate = hdr['SWCREATE']
                     except :
                         swcreate = 'xxxx'
                     hdul.close()
+
                     if swcreate.startswith ('N.I.N.A.'):
                         seqNbr += 1
                         imagename = gen_seqNbr (imgname, seqNbr)
-                        newpath = str(ip_dir)+'\\'+ imagename
-                        #print('NEWPATH= '+newpath)
+                        newpath = str(ip_dir)+'\\'+ imagename 
                         try:
                             os.rename (inpath, newpath)
                             inpath = newpath
                         except:
-                            logger.error("RENAME ERROR " + imgname)
-                            
+                            logger.error("RENAME ERROR " + imgname)                            
                     
                     # make sure this is a "LIGHT" file needing plate-solving
-                    if (imageType.startswith ('LIGHT')):
+                    if (imageType.startswith ('LIGHT')):                        
                         try:
                             # Filenames with embedded spaces must be enclosed in " marks. The "update" option causes 
                             # the image header to be updated with the WCS information.
@@ -209,22 +211,23 @@ def add_WCS_Coordinates (ip_dir, wcs_dest, nowcs_dest, flatpath):
                             code = str(res).split('=')
                             logger.info('ASTAP Result= '+ code[2])
                         except Exception as e:
-                            logger.error("ASTAP EXECUTION ERROR " + str(e) +'\n')
-                        
-                        calinpath = inpath.replace('MN ', 'MNc ')
-                        #print('calinpath= '+calinpath)
-                        shutil.copy2(inpath, calinpath)
+                            logger.error("ASTAP EXECUTION ERROR " + str(e) +'\n')                  
                     
                         if code[2] == '0)':
                             logger.info('PLATESOLVE SUCCESS- '+ inpath)
                             try:
                                 # perform image calibration...
-                                res = calibrateImage (calinpath, flatpath)
+                                calinpath = inpath.replace('MN ', 'MNc ')
+                                shutil.copy2(inpath, calinpath) 
+                                
+                                res = calibrate (ip_dir, calinpath, dark_exp, dark_binning, bias, mstr_dark)
                                 logger.info('Calibrate Result= ' +str(res))
                             except:
                                 res = 1
                                 logger.error("Calibration Error, File: "+ calinpath)
-                            #print("CalERROR = "+str(res))
+
+                            logger.info("CalSTATUS = "+str(res))
+
                             if (res == 0):
                                 shutil.move (calinpath, wcs_dest)
                             else:
@@ -244,8 +247,9 @@ def add_WCS_Coordinates (ip_dir, wcs_dest, nowcs_dest, flatpath):
                         try:
                             shutil.move (inpath, nowcs_dest)
                         except:
-                            logger.info(inpath + ' alreary exists')
-                            
+                            logger.info(inpath + ' already exists')
+                            os.remove(inpath)
+
                 except Exception as e:
                     logger.error("ERROR 000 " + inpath + " " + str(e) +'\n')
                     shutil.move (inpath, nowcs_dest)
@@ -256,43 +260,45 @@ def add_WCS_Coordinates (ip_dir, wcs_dest, nowcs_dest, flatpath):
         with open (ip_dir+'\\SEQ_NBR\\nina_seqNbr.txt', 'w') as fs:
             fs.write(str(seqNbr))
             logger.info('FINAL SeqNbr = '+str(seqNbr))
-        
+        return 0
         
     except Exception as e:
         logger.error ("MAIN BODY ERROR *** " + str(e) +'\n')
         with open (ip_dir+'\\SEQ_NBR\\nina_seqNbr.txt', 'w') as fs:
             fs.write(str(seqNbr))
-
+        return 1  #sys.exit(1)
 #================================================================================================================
 #================================================================================================================
 
 logger = logging.getLogger('NINA_OROCESSING')   #(__name__)
-logging.basicConfig(filename="C:\\PLT-SLV\\WCS_SeqNbr.log",format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=logging.DEBUG)
+logging.basicConfig(filename="C:\\PLT-SLV\\Plt-Slv_log.txt",format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=logging.DEBUG)
 
 logger.info("\nSTARTING\n")
 
 try:
     if (len(sys.argv) != 2) :
         logger.error("\n**** Add Path for the input files to be processed! ****\n")
+        print("\n**** Add Path for the input files to be processed! ****\n")
         sys.exit (1)
     
-    today = datetime.now()
+    today = datetime.today() - timedelta(hours=12)
     foldername = today.strftime("%Y-%m-%d")
 
     wcs_dest = "R:\\Eagle\\SkyX\\images\\"+foldername+'\\'
- #   wcs_dest = sys.argv[1]+'\\WCS\\'                 # **** For Test Purposes!
+  #  wcs_dest = sys.argv[1]+'\\WCS\\'                 # **** For Test Purposes!
 
     nowcs_dest = "R:\\Eagle\\SkyX\\images\\"+foldername+'\\'
- #   nowcs_dest = sys.argv[1]+'\\ERR\\'               # **** For Test Purposes!
+  #  nowcs_dest = sys.argv[1]+'\\ERR\\'               # **** For Test Purposes!
     if not os.path.isdir(wcs_dest) :
          pathlib.Path(wcs_dest).mkdir(parents=True, exist_ok=True)
          
     if not os.path.isdir(nowcs_dest) :
          pathlib.Path(nowsc_dest).mkdir(parents=True, exist_ok=True)
-    flatpath = sys.argv[1] + '\\' + 'CalFlats'
-    add_WCS_Coordinates (sys.argv[1], wcs_dest, nowcs_dest, flatpath)
-    
+
+    res = add_WCS_Coordinates (sys.argv[1], wcs_dest, nowcs_dest)
+    logger.info ("End Session with res = " + str(res))
+
 except Exception as e:
         logger.error("ERROR *** " + str(e) +'\n')
-        
+        sys.exit(1)
 
